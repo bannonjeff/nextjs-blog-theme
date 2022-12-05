7b026793d9370c8b38bb42f50f1a10e492a2eaaa
bannonjeff <119867468+bannonjeff@users.noreply.github.com>
2022-12-05 13:49:38 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,6 +4,8 @@ export async function middleware(request) {
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-foobar', 'true');
 
+  console.log("middleware");
+  
   return NextResponse.next({
     request: {
       headers: requestHeaders
